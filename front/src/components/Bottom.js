01fb97a6f6e3a7764ed8a9343b4b52ba55ab3914
ongheong <pudding4271@gmail.com>
2023-10-09 11:13:54 +0900

    Squashed commit of the following:
    commit ff21853ab36c2b8515d8fddc0c1755d2e000a250
    Author: ongheong <pudding4271@gmail.com>
    Date:   Mon Oct 9 11:12:17 2023 +0900
    
        feat: EnterPage 수정 및 InputPage 추가
    
    commit 966280d46953b049051898809bbf63d239a0579a
    Author: ongheong <pudding4271@gmail.com>
    Date:   Wed Oct 4 14:04:26 2023 +0900
    
        feat: 프론트 브랜치 재정립 및 입장페이지 추가

---
+++ front/src/components/Bottom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/Bottom.css';
+
+function Bottom() {
+    return (
+        <div className='BottomBox'>
+            <div className='BottomContent'>@2023 Twenty4</div>
+        </div>
+    );
+}
+
+export default Bottom;
