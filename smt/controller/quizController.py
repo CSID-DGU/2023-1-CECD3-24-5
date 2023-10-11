0a9d6ea44d5528c8458db1793ac2d03fe553cb10
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2023-10-11 15:21:23 +0900

    refactor: mvc 패턴 적용

---
+++ smt/controller/quizController.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request, render_template
+
+import sys
+import os
+
+# 현재 스크립트의 경로를 가져옴
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(os.path.join(current_path, '../service'))
+from quizService import *
+
+app = Flask(__name__)
+
+
+#템플릿 API
+@app.route('/home', methods=['GET'])
+def fetchHome():
+    return render_template('home.html')
+
+@app.route('/generate_page', methods=['GET'])
+def FetchGeneratePage():
+    return render_template('create.html')
+
+@app.route('/solve_page', methods=['GET'])
+def fetchSolvePage():
+    return render_template('solve.html')
+
+#문제 생성 API
+@app.route('/create/quiz', methods=['GET'])
+def fetchQuizsController():
+    scope = request.args.get('scope')
+    number = request.args.get('number')
+    if scope is None or number is None:
+        return 'scope와 number 쿼리 매개변수가 필요합니다.'
+    
+    return jsonify(
+        CreateQuizsService.createQuizs(
+            scope,
+            number
+            )
+        )
