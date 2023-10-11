498af7236be0248b7a3a93fa08120909685ff324
DongKyeomJang <jangelliot0404@dgu.ac.kr>
2023-10-11 11:50:22 +0900

    refactor: 파일구조 수정, 모듈화

--- smt/main.py
+++ smt/main.py
@@ -1,4 +1,19 @@
-from smt.problem_BstHeapNormalTree import BstHeapNormalTree
+from quiz import quiz_bsort
+from quiz import quiz_BstHeapNormalTree
 
-one=BstHeapNormalTree()
-one.makeQuiz()
+one=quiz_bsort()
+two=quiz_BstHeapNormalTree()
+quiz=[one,two]
+
+for i in quiz:
+    quiz[i].setQuiz()
+    print('문제:')
+    print(quiz[i].problem)
+    print('선지:')
+    print('1:'+quiz[i].quiz.select[0])
+    print('2:'+quiz[i].quiz.select[1])
+    print('3:'+quiz[i].quiz.select[2])
+    print('4:'+quiz[i].quiz.select[3])
+    print('답:')
+    print(quiz[i].quiz.select[0])
+
--- smt/source/structure/structure_BST.py
+++ smt/source/structure/structure_BST.py
@@ -1,6 +1,16 @@
 from z3 import *
 import random
-from smt.structure_Node import Node
+from structure_Node import Node
+
+def generate_bst_conditions(node):
+        conditions = []
+        if node.left:
+            conditions.append(node.left.value < node.value)
+            conditions.extend(generate_bst_conditions(node.left))
+        if node.right:
+            conditions.append(node.right.value > node.value)
+            conditions.extend(generate_bst_conditions(node.right))
+        return conditions
 
 def collect_all_nodes(node):    
     nodes = [node]
@@ -9,28 +19,11 @@ def collect_all_nodes(node):
     if node.right:
         nodes.extend(collect_all_nodes(node.right))
     return nodes
+        
 
-def generate_not_bst_conditions(node):
-    conditions=[]
-    if node.left:
-        conditions.append(node.left.value > node.value)
-        conditions.extend(node.left)
-    if node.right:
-        conditions.append(node.right.value < node.value)
-        conditions.extend(node.right)
-    return conditions
+class BST:
 
-def generate_not_minHeap_conditions(node):
-    conditions=[]
-    if node.left:
-        conditions.append(node.left.value > node.value)
-        conditions.extend(node.left)
-    if node.right:
-        conditions.append(node.right.value > node.value)
-        conditions.extend(node.right)
-    return conditions
 
-class NormalTree:
     def __init__(self):
         self.a = Node(Int('a'))
         self.b = Node(Int('b'))
@@ -55,29 +48,28 @@ class NormalTree:
         all_nodes = collect_all_nodes(self.a)
         unique_conditions = Distinct(*[node.value for node in all_nodes])
 
-        # bst 위반
-        not_bst_conditions = Or(generate_not_bst_conditions(self.a))
-        not_heap_conditions =Or(generate_not_minHeap_conditions(self.a))
 
+        #BST 조건
+        bst_conditions = And(generate_bst_conditions(self.a))
         s = Solver()
 
-        normalTree_models = []
+        bst_models = []
 
         # 아래 모델을 여러개 뽑는 이유는, z3가 같은 해를 출력하는 경향이 있기 때문. 10개의 해를 뽑아놓고 거기서 랜덤하게 선택하도록 함으로써 해결.
         # 이진 탐색 트리 조건 확인
         s.push()
-        s.add(And(not_bst_conditions,not_heap_conditions, unique_conditions))
+        s.add(And(bst_conditions, unique_conditions))
         for _ in range(10):  # 10개의 서로 다른 모델 뽑아냄
             if s.check() == sat:
                 m = s.model()
-                normalTree_models.append(m)
+                bst_models.append(m)
                 # 이미 찾은 모델을 제외하고 다시 뽑아냄
                 s.add(Or([var != m[var] for var in [self.a.value, self.b.value, self.c.value, self.d.value, self.e.value, self.f.value, self.g.value, self.h.value, self.i.value]]))
         s.pop()
 
 
         # 가능한 해 중 하나를 무작위로 선택
-        selected_model = random.choice(normalTree_models)
+        selected_model = random.choice(bst_models)
 
         self.a=selected_model[self.a.value]
         self.b=selected_model[self.b.value]
