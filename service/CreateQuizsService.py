348f5ee020863aac67ec8619dc89ef859e530613
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2023-10-31 07:58:47 +0900

    feat:data(quiz)추가 및 application.py기준 경로 연결

--- service/CreateQuizsService.py
+++ service/CreateQuizsService.py
@@ -1,15 +1,29 @@
-from util.Quiz import *
+from service.data.quiz.quiz_bsort import *
+from service.data.quiz.quiz_BstHeapNormalTree import *
+from service.data.quiz.quiz_PostfixPrefix import *
+from service.data.quiz.quiz_WorstComplexity import *
+
 
 class CreateQuizsService:
-    def createQuizs(scope, number):
-        quizes = []
+
+    def __init__(self):
+        self.quizes=[]
+        self.candidate=[]
+        bsort=quiz_bsort()
+        BSTHeapNormal=quiz_BstHeapNormalTree()
+        PostfixPrefix=quiz_PostfixPrefix()
+        WorstComplexity=quiz_WorstComplexity()
+        self.candidate.append(bsort)
+        self.candidate.append(BSTHeapNormal)
+        self.candidate.append(PostfixPrefix)
+        self.candidate.append(WorstComplexity)
+
+    def createQuizs(self, number):
         for i in range(0, int(number)):
-            #문제 생성 함수()
-            quiz = Quiz()
-            quiz.setQuiz(1, 'test', [1,2,3,4], 0)
+            self.candidate[i].setQuiz(i+1)
+            quiz=self.candidate[i].quiz
             #여기까지 문제 생성
             quiz.mixAnswer()
             #직렬화
-            quizes.append(quiz.changToDict())
-        return quizes
-
+            self.quizes.append(quiz.changToDict())
+        return self.quizes
