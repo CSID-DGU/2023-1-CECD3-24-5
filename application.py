b0fe81e99a03e75a76ebd0b7816e018dd7db9d7c
ongheong <pudding4271@gmail.com>
2023-11-01 15:18:41 +0900

    fix: 퀴즈박스 height 수정, 문제 생성 api 재작성

--- application.py
+++ application.py
@@ -1,36 +1,39 @@
 from flask import Flask, jsonify, request, render_template
 from service.CreateQuizsService import *
+from flask_cors import CORS
 
-app = Flask(__name__)
+import sys
+import os
 
+# 현재 스크립트의 경로를 가져옴
+current_path = os.path.dirname(os.path.abspath(__file__))
 
-#템플릿 API
-@app.route('/home', methods=['GET'])
-def fetchHome():
-    return render_template('home.html')
+sys.path.append(os.path.join(current_path, 'service'))
 
-@app.route('/generate_page', methods=['GET'])
-def FetchGeneratePage():
-    return render_template('create.html')
+#app에서 static 폴더를 React의 build 폴더로 설정
+app = Flask(__name__, static_folder='front/build', static_url_path='/')
+CORS(app)
 
-@app.route('/solve_page', methods=['GET'])
-def fetchSolvePage():
-    return render_template('solve.html')
+#메인 페이지 라우팅
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
 
 #문제 생성 API
 @app.route('/create/quiz', methods=['GET'])
 def fetchQuizsController():
-    scope = request.args.get('scope')
-    number = request.args.get('number')
-    if scope is None or number is None:
+    number = request.args.get('number', default=1, type=int)
+
+    if number is None:
         return 'scope와 number 쿼리 매개변수가 필요합니다.'
     
-    return jsonify(
-        CreateQuizsService.createQuizs(
-            scope,
-            number
-            )
-        )
+    #CreateQuizsService의 인스턴스 생성
+    quiz_service = CreateQuizsService()
+
+    quizes = quiz_service.createQuizs(number)
+    
+    return jsonify(quizes)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
