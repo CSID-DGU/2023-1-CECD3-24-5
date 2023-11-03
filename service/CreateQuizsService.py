b6e5824153b4add80c9fdbc3c96e9e86b5b9d2fe
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2023-11-04 02:26:18 +0900

    feature: quiz_circularQueue.py 순환 큐 응용문제 생성

--- service/CreateQuizsService.py
+++ service/CreateQuizsService.py
@@ -2,7 +2,7 @@ from service.data.quiz.quiz_bsort import *
 from service.data.quiz.quiz_BstHeapNormalTree import *
 from service.data.quiz.quiz_PostfixPrefix import *
 from service.data.quiz.quiz_WorstComplexity import *
-
+from service.data.quiz.quiz_circularQueue import *
 
 class CreateQuizsService:
 
@@ -13,10 +13,12 @@ class CreateQuizsService:
         BSTHeapNormal=quiz_BstHeapNormalTree()
         PostfixPrefix=quiz_PostfixPrefix()
         WorstComplexity=quiz_WorstComplexity()
+        circularQueue=quiz_circularQueue()
         self.candidate.append(bsort)
         self.candidate.append(BSTHeapNormal)
         self.candidate.append(PostfixPrefix)
         self.candidate.append(WorstComplexity)
+        self.candidate.append(circularQueue)
 
     def createQuizs(self, number):
         for i in range(0, int(number)):
