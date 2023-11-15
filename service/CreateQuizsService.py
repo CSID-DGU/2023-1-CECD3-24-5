16517e9d0a03376e983cd72cf695d46c354bb8a9
ongheong <pudding4271@gmail.com>
2023-11-15 20:37:56 +0900

    Squashed commit of the following:
    commit bc2c09e29401bd614d82feac26ab565ae70a4e3d
    Merge: 11c2cba2 dbc5e815
    Author: ongheong <87983309+ongheong@users.noreply.github.com>
    Date:   Wed Nov 15 20:31:27 2023 +0900
    
        Merge pull request #32 from CSID-DGU/master
    
        master 변경사항 -> front_build에 병합
    
    commit dbc5e815e08b57aee1dd8d7736c6bc0ee9f73e05
    Merge: 9b8352de ed283e18
    Author: 장동겸 <86873281+dongkyeomjang@users.noreply.github.com>
    Date:   Wed Nov 15 19:32:39 2023 +0900
    
        Merge pull request #30 from dongkyeomjang/master
    
        순환큐 문제 추가
    
    commit ed283e18b65fcfea6cdafcd24aa00b1d40d94ad9
    Author: dongkyeomjang <jangelliot0404@dgu.ac.kr>
    Date:   Wed Nov 15 19:30:31 2023 +0900
    
        feat:순환큐 문제 2개 추가
    
    commit b6e5824153b4add80c9fdbc3c96e9e86b5b9d2fe
    Author: dongkyeomjang <jangelliot0404@dgu.ac.kr>
    Date:   Sat Nov 4 02:26:18 2023 +0900
    
        feature: quiz_circularQueue.py 순환 큐 응용문제 생성
    
    commit 9b8352de2285ef5487532a1b19d9832383ec924c
    Merge: 642d72b0 ebc8d94d
    Author: 장동겸 <86873281+dongkyeomjang@users.noreply.github.com>
    Date:   Sat Nov 4 00:42:06 2023 +0900
    
        Merge pull request #29 from dongkyeomjang/master
    
        build: front 빌드 최신화
    
    commit ebc8d94d2e0af276b0f583ca59c2e24c34ce7ed6
    Author: dongkyeomjang <jangelliot0404@dgu.ac.kr>
    Date:   Sat Nov 4 00:41:24 2023 +0900
    
        build: front 빌드 최신화

--- service/CreateQuizsService.py
+++ service/CreateQuizsService.py
@@ -2,7 +2,8 @@ from service.data.quiz.quiz_bsort import *
 from service.data.quiz.quiz_BstHeapNormalTree import *
 from service.data.quiz.quiz_PostfixPrefix import *
 from service.data.quiz.quiz_WorstComplexity import *
-
+from service.data.quiz.quiz_circularQueueF import *
+from service.data.quiz.quiz_circularQueueE import *
 
 class CreateQuizsService:
 
@@ -13,10 +14,14 @@ class CreateQuizsService:
         BSTHeapNormal=quiz_BstHeapNormalTree()
         PostfixPrefix=quiz_PostfixPrefix()
         WorstComplexity=quiz_WorstComplexity()
+        circularQueueF=quiz_circularQueueF()
+        circularQueueE=quiz_circularQueueE()
         self.candidate.append(bsort)
         self.candidate.append(BSTHeapNormal)
         self.candidate.append(PostfixPrefix)
         self.candidate.append(WorstComplexity)
+        self.candidate.append(circularQueueF)
+        self.candidate.append(circularQueueE)
 
     def createQuizs(self, number):
         for i in range(0, int(number)):
